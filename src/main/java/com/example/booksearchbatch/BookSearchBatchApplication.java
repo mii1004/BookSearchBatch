1f00b6cd6142f748ee22e227b7a107ea2fa03bc8
mii1004 <mii.1004@docomo.ne.jp>
2019-11-28 12:53:30 +0900

    create project

---
+++ src/main/java/com/example/booksearchbatch/BookSearchBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksearchbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearchBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booksearchbatch/BookSearchBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksearchbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
